2c03ba41fa768c9fae32fdf52c39e52c90ff25ad
Zakhar2004 <92360164+Zakhar2004@users.noreply.github.com>
2021-11-17 17:03:54 +1000

    Add files via upload
    Калькулятор с циклом for

--- zikher/src/com/company/Main.java
+++ zikher/src/com/company/Main.java
@@ -6,30 +6,34 @@ package com.company;
 
 
 
+
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("1.Умножение");
-        System.out.println("2.Сложение");
-        System.out.println("3.Вычитанеи.");
-        System.out.println("4.Деление");
+        public static void main(String[] args) {
+            System.out.println("1.Умножение");
+            System.out.println("2.Сложение");
+            System.out.println("3.Вычитанеи.");
+            System.out.println("4.Деление");
 
 
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            int operation = scanner.nextInt();
-            if (operation > 4) {
-                System.out.println("Не удовлетворенео условиею");
-            } else if (operation < 1) {
-                System.out.println("Не удовлетворено условие.");
-            } else {
-                System.out.println("Введите первое число:");
+            for (int i = 0; i < 100; i++) {
+
+                Scanner scanner = new Scanner(System.in);
+                int operation = scanner.nextInt();
+                if (operation > 4 || operation < 1) {
+                    System.out.println("Условие введено неправильно!");
+                    continue;
+                }
+                System.out.println("Введите первое число : ");
                 int a = scanner.nextInt();
-                System.out.println("Введите второе число:");
+                System.out.println("Введите второе число: ");
                 int b = scanner.nextInt();
                 int result;
+                String error;
+
 
                 if (operation == 1) {
                     result = a * b;
@@ -37,23 +41,20 @@ public class Main {
                     result = a + b;
                 } else if (operation == 3) {
                     result = a - b;
-                } else {
+                } else   {
                     result = a / b;
                 }
-                System.out.println("Результат = " + result);
+
+                 System.out.println("Результат = " + result);
                 scanner.close();
                 break;
             }
         }
     }
-}
 
 
 
 
-//region task_1
-/*1. Дописать калькулятор,чтобы он мог выполнять деление
-2.Чтобы калькулятор приведении значения переменной "operation",отличного от заданных условий
- выдавал в консоль об ошибке и предлагал заново ввести значение
- */
-//endregion
+
+
+
